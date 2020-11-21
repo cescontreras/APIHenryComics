e51f4b962b238328c157ef76e9dc05c6bfbe1019
cescontreras <contreras-cesaralejandro@hotmail.com>
2020-11-21 16:22:02 -0300

    api commit

--- src/db.js
+++ src/db.js
@@ -2,15 +2,14 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env; 
+const { DATABASE_URL } = process.env; 
 const bcrypt = require("bcrypt");
 
  
 const sequelize = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
+	`${DATABASE_URL}`,
 	{
-		logging: false, // set to console.log to see the raw SQL queries
-		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+		logging: false, // set to console.log to see the raw SQL queries		
 	}
 );
 const basename = path.basename(__filename);
