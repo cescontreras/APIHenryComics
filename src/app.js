611bfb5656ba101054e0be5f89c73fab244bad45
cescontreras <contreras-cesaralejandro@hotmail.com>
2020-11-27 23:57:13 -0300

    update

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ require('./db.js');
 const server = express();
 
 server.use(cors({
-  origin: "https://henry-comics.vercel.app/",
+  origin: "https://henry-comics.vercel.app",
   credentials: true,
 }));
 
@@ -24,7 +24,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://henry-comics.vercel.app/'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://henry-comics.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
